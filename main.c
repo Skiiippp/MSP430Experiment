f87dce5ab0bd3bde14e3b77ed4bdd2ad8d806f9e
James Gruber <54287783+Skiiippp@users.noreply.github.com>
2022-08-25 15:59:28 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,113 +1,122 @@
 // These tutorials helped: 
 // https://www.allaboutcircuits.com/technical-articles/how-to-a-162-lcd-module-with-an-mcu/
 // https://openlabpro.com/guide/custom-character-lcd-pic/
+// https://svijaykoushik.github.io/blog/2019/10/04/three-awesome-ways-to-generate-random-number-in-javascript/
 
 
 #include <msp430g2553.h>
 
 
+
+//constants to control the three control pins
 const unsigned char RS_Pin = 0x01;
 const unsigned char RW_Pin = 0x02;
 const unsigned char E_Pin = 0x04;
 
-unsigned char dino1[] = {0x06, 0x05, 0x07, 0x06, 0x0F, 0x16, 0x15, 0x05};
-unsigned char dino2[] = {0x06, 0x05, 0x07, 0x06, 0x0F, 0x16, 0x12, 0x02}; 
-unsigned char cactus[] = {0x04, 0x14, 0x15, 0x1D, 0x07, 0x04, 0x04, 0x04};
+unsigned char board[16][20];
 
+long int X;
+long int a = 1664525;
+long int c = 1013904223;
+long long int m = 4294967296;
 
-int score = 0;
-
-
-void stupidDelay();
-void writeInstruction(unsigned char insCode);
-void writeData(unsigned char dataCode);
-void initializeScreen();
-void clear();
-void customChar(unsigned char *charArray, const char loc);
-void demoAnimation();
-void newFrame();
-void printScore();
-void initGame();
-
-
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD; 
-	initializeScreen();
-	customChar(dino1, 0);
-	customChar(dino2, 1);
-	customChar(cactus, 2);
-	writeInstruction(0xC8);
-	writeData(2);
-	score = 246;
-	printScore();
-
+int random(){
+	X = (a * X + c) % m;
+    return X;
 }
 
-void initGame(){
-
-}
-
-void printScore(){
-	int ones = score % 10;
-	int tens = ((score % 100) - ones) / 10;
-	int hundreads = (score - (score % 100)) / 100; 
-	writeInstruction(0x80);
-	writeData(0x30 + hundreads);
-	writeInstruction(0x81);
-	writeData(0x30 + tens);
-	writeInstruction(0x82);
-	writeData(0x30 + ones);
-}
-
-void newFrame(){
-	for(int j = 0; j <20000; j++);
-	writeInstruction(0x01);
-	stupidDelay();
-}
-
-void stupidDelay(){
-	for(int i = 0; i < 100; i++);
-}
-
-void writeInstruction(unsigned char insCode){
-	stupidDelay();
-	P1OUT &= ~RS_Pin;
-	P1OUT &= ~RW_Pin;
-	P1OUT &= ~E_Pin;
-	P2OUT = insCode;
-	P1OUT |= E_Pin;
-	P1OUT &= ~E_Pin;
-}
-
-void writeData(unsigned char dataCode){
-	stupidDelay();
-	P1OUT |= RS_Pin;
-	P1OUT &= ~RW_Pin;
-	P1OUT &= ~E_Pin;
-	P2OUT = dataCode;
-	P1OUT |= E_Pin;
-	P1OUT &= ~E_Pin;
-
+void setBoardStartPos(){
+	
+	for(int i = 0; i < 16; i++){
+		for(int j = 0; j < 20; j++){
+			int rand = random();
+			if(rand % 3 == 0){
+				board[i][j] = 'A';
+			} else{
+				board[i][j] = 'D';
+			}
+		}
+	}
 	
 }
 
-void initializeScreen(){
-	stupidDelay();
-	P1SEL &= 0x00;
-	P2SEL &= 0x00;
-	P1DIR |= 0xFF;
-	P2DIR |= 0xFF;
+void delayUpdate(){
+	__delay_cycles(16000);
+}
+
+void writeInstruction(unsigned char instruction){
+	delayUpdate();
+	P1OUT &= ~RS_Pin;	//Set register select to low for instruction
+	P1OUT &= ~E_Pin; //Set enable pin low;
+	P2OUT = instruction;
+	P1OUT |= E_Pin; //Set enable pin high the low to generate falling edge
+	P1OUT &= ~E_Pin;
+}
+
+void initialize(){
+	WDTCTL = WDTPW + WDTHOLD;
+	P1SEL = 0x00;
+	P2SEL = 0x00;
+	P1DIR = 0xFF;
+	P2DIR = 0xFF;
+	delayUpdate();
+	P1OUT &= ~RW_Pin; //Hold read/write pin low, since will always be writing 
 	writeInstruction(0x01);
 	writeInstruction(0x02);
 	writeInstruction(0x0C);
 	writeInstruction(0x38);
 }
 
-void clear(){
-	stupidDelay();
-	writeInstruction(0x01);
-	writeInstruction(0x02);
+void writeData(unsigned char data){
+	delayUpdate();
+	P1OUT |= RS_Pin;
+	P1OUT &= ~E_Pin;
+	P2OUT = data;
+	P1OUT |= E_Pin;
+	P1OUT &= ~E_Pin;
+}
+
+//function to determine if each cell will be alive or dead after the next generation
+void aliveOrDead(int y, int x){
+    int count = 0;
+    if (y > 0 && x > 0 && board[y-1][x-1] == 'A' || board[y-1][x-1] == 'B'){
+        count ++;
+    }
+    if (y > 0 && board[y-1][x] == 'A' || board[y-1][x] == 'B'){
+        count ++;
+    }
+    if (y > 0 && x < 19 && board[y-1][x+1] == 'A' || board[y-1][x+1] == 'B'){
+        count ++;
+    }
+    if (x > 0 && board[y][x-1] == 'A' || board[y][x-1] == 'B'){
+        count ++;
+    }
+    if (x < 19 && board[y][x+1] == 'A' || board[y][x+1] == 'B'){
+        count ++;
+    }
+    if (y < 15 && x > 0 && board[y+1][x-1] == 'A' || board[y+1][x-1] == 'B'){
+        count ++;
+    }
+    if (y < 15 && board[y+1][x] == 'A' || board[y+1][x] == 'B'){
+        count ++;
+    }
+    if (y < 15 && x < 19 && board[y+1][x+1] == 'A' || board[y+1][x+1] == 'B'){
+        count ++;
+    }
+
+    if (board[y][x] == 'A' && (count < 2 || count > 3)){
+        board[y][x] = 'A' + 0x01;
+    } else if (board[y][x] == 'D' && count == 3){
+        board[y][x] = 'D' + 0x01;
+    }
+}
+
+void next_gen(){
+    for (int i = 0; i < 16; i++){
+        for (int j = 0; j < 20; j++){
+            aliveOrDead(i,j);
+        }
+    }
 }
 
 void customChar(unsigned char *charArray, const char loc){
@@ -117,21 +126,59 @@ void customChar(unsigned char *charArray, const char loc){
 	}
 }
 
-void demoAnimation() {
-	unsigned char dino1[] = {0x06, 0x05, 0x07, 0x06, 0x0F, 0x16, 0x15, 0x05};
-	unsigned char dino2[] = {0x06, 0x05, 0x07, 0x06, 0x0F, 0x16, 0x12, 0x02}; 
-	customChar(dino1, 0);
-	customChar(dino2, 1);
-	for(int i = 0; i < 17; i++){
-		newFrame();
-		writeInstruction(0xC0 + i);
-		if(i % 2 == 0){
-			writeData(0);
-		} else{
-			writeData(1);
+int twoPower(int power){
+	int sum = 1;
+	for(int i = 0; i < power; i++){
+		sum = sum * 2;
+	}
+	return sum;
+}
+
+void drawBoard(){
+
+	for(int g = 0; g < 2; g++){
+		for(int h = 0; h < 4; h++){
+			unsigned char toDraw[8];
+			for(int i = g * 8; i < 8 + g * 8; i++){
+				unsigned char sum = 0;
+				for(int j = h * 5; j < 5 + h * 5; j++){
+					if(board[i][j] == 'B'){
+						board[i][j] = 'D';
+					}
+					if(board[i][j] == 'E'){
+						board[i][j] = 'A';
+					}
+					if(board[i][j] == 'A'){
+						sum += twoPower(4 - j % 5);
+					}
+				}
+				toDraw[i % 8] = sum;
+			}
+
+			customChar(toDraw, h + g * 4);
+			if(g == 0){
+				writeInstruction(0x86 + h);
+			} else{
+				writeInstruction(0xC6 + h);
+			}
+			writeData(h + g * 4);
 		}
 	}
+}
+
+
+
+int main(){
+	initialize();
+	setBoardStartPos();
+	while(1){
+		drawBoard();
+		next_gen();
+	}
+
+
 	
+	return 0;
 }
 
 
